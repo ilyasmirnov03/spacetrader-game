28c80db970fdd78f276d566765de6092affc9cd6
Ilya Smirnov <ilua.smirnof@gmail.com>
2023-12-06 16:27:55 +0100

    feat(Router): Add router to application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
-import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
