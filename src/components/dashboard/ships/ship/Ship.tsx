b2aa3a96636ca491f5242ea4bbf56cd5ea56bb2f
Ilya Smirnov <ilua.smirnof@gmail.com>
2023-12-07 17:34:41 +0100

    feat(Ship): Added page for ship details
    Added page for ship details

---
+++ src/components/dashboard/ships/ship/Ship.tsx
@@ -0,0 +1,47 @@
+import {FC, useEffect, useState} from 'react';
+import {ShipModel} from '../../../../models/ship.model.ts';
+import {useLocation, useParams} from 'react-router-dom';
+import axios from 'axios';
+import environment from '../../../../constants/environment.const.ts';
+import {ApiResponse} from '../../../../models/api-response.ts';
+interface ShipProps {
+}
+
+export const Ship: FC<ShipProps> = () => {
+
+    const [ship, setShip] = useState<ShipModel>();
+
+    // State passed from clicked button, can be undefined if request didn't come from button click
+    const { state } = useLocation();
+
+    // Id from url params
+    const { shipId } = useParams();
+
+    function getShip(): void {
+        axios.get(`${environment.baseUrl}/my/ships/${shipId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${environment.loginToken}`
+            },
+        }).then((data) => {
+            setShip((data.data as ApiResponse).data as ShipModel | undefined);
+        });
+    }
+
+    useEffect(() => {
+        if (state?.ship) {
+            setShip(state.ship as ShipModel);
+        } else {
+            getShip();
+        }
+    }, []);
+
+
+    return (
+        <section>
+            <h2>{ship?.symbol}</h2>
+            <p>Fuel: {ship?.fuel.current} / {ship?.fuel.capacity}</p>
+            <progress value={ship?.fuel.current} max={ship?.fuel.capacity}></progress>
+        </section>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { Dashboard } from './components/dashboard/root/Dashboard.tsx';
 import { Ships } from './components/dashboard/ships/Ships.tsx';
 import environment from './constants/environment.const.ts';
 import { LocalStorageEnum } from './enum/local-storage.enum.ts';
+import {Ship} from './components/dashboard/ships/ship/Ship.tsx';
 
 const loginToken = localStorage.getItem(LocalStorageEnum.LOGIN_KEY);
 environment.loginToken = loginToken ?? '';
@@ -22,7 +23,11 @@ const router = createBrowserRouter([
       },
       {
         path: '/ships',
-        element: <Ships></Ships>
+        element: <Ships></Ships>,
+      },
+      {
+        path: '/ships/:shipId',
+        element: <Ship></Ship>
       }
     ]
   },
