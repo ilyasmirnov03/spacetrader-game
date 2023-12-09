9434bb16651f1f7cc6729eb6e6f994fd6b3da893
Ilya Smirnov <ilua.smirnof@gmail.com>
2023-12-09 17:40:44 +0100

    feat(Login): Interface if logged in

--- src/components/login/root/Login.tsx
+++ src/components/login/root/Login.tsx
@@ -1,5 +1,6 @@
-import { FC, FormEvent } from 'react';
+import {FC, FormEvent} from 'react';
 import {useAuth} from '../../../hooks/auth/useAuth.tsx';
+import {Link} from 'react-router-dom';
 
 interface LoginProps {
 }
@@ -23,9 +24,15 @@ export const Login: FC<LoginProps> = () => {
         <div>
             <form onSubmit={login}>
                 <input onInput={(e) => setToken(e.currentTarget.value)} type="text" name="token"
-                    placeholder="Place or type or token here" />
-                <input type="submit" value={'Login'} />
+                       placeholder="Place or type or token here"/>
+                <input type="submit" value={'Login'}/>
             </form>
+            {auth.token && (
+                <>
+                    <p>You are already logged in.</p>
+                    <Link className="button" to="/dashboard">Go to dashboard</Link>
+                </>
+            )}
         </div>
     );
 };
