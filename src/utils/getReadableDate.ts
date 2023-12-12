3745dae84f4b18bd9b02d3470a029481a87d86db
Ilya Smirnov <ilua.smirnof@gmail.com>
2023-12-12 17:15:43 +0100

    refactor(Ship): Always two digits in a date

--- src/utils/getReadableDate.ts
+++ src/utils/getReadableDate.ts
@@ -1,3 +1,12 @@
+function singleDigitToDoubleDigits(n: number): string {
+    return n.toLocaleString(undefined, { minimumIntegerDigits: 2 });
+}
+
 export function getReadableDate(date: Date): string {
-    return `${date.getDate()}/${date.getMonth() + 1} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = singleDigitToDoubleDigits(date.getHours());
+    const minutes = singleDigitToDoubleDigits(date.getMinutes());
+    const seconds = singleDigitToDoubleDigits(date.getSeconds());
+    const day = singleDigitToDoubleDigits(date.getDate());
+    const month = singleDigitToDoubleDigits(date.getMonth() + 1);
+    return `${day}/${month} ${hours}:${minutes}:${seconds}`;
 }
